f7b876954b0b231e0ffad24c15665b46e00e5e9c
Hienz <francispanogao@gmail.com>
2026-01-18 17:51:02 +0800

    Initialize admin dashboard module

---
+++ Admin_Screen/Dasgboard_page/Dashboard.java
@@ -0,0 +1,27 @@
+package Admin_Screen.Dasgboard_page;
+
+import Main_System.MainFrame;
+import java.awt.Dimension;
+import javax.swing.*;
+
+
+public class Dashboard extends javax.swing.JPanel {
+    private MainFrame frame;
+
+    public Dashboard(MainFrame frame){
+        this.frame = frame;
+        panel();
+    }
+    public void panel(){
+        this.setLayout(null);
+        this.setPreferredSize(new Dimension(1512, 982));
+        
+        JLabel background = new JLabel();
+        background.setBounds(0,0,1512,982);
+        background.setIcon(new ImageIcon("Images\\Admin_Book Management.png"));
+        background.setLayout(null);
+
+        this.add(background);
+    }
+    
+}
