cde2a8e56db2e3add570f651b06ef91ce8d1462c
Hienz <francispanogao@gmail.com>
2026-01-23 19:33:22 +0800

    Update Data.java

--- Login_screen/Data.java
+++ Login_screen/Data.java
@@ -14,4 +14,29 @@ public class Data{
         users.add(new User(first, last, email, id, password));
         return true;
     }
+
+    public static User login(String input, String password){
+        for(User user: users){
+            if(user.getUsername().equalsIgnoreCase(input) ||
+            user.getEmail().equalsIgnoreCase(input)){
+                if(user.getPassword().equals(password)){
+                    return user;
+                }else{
+                    return null;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static boolean userExists(String input){
+        for(User user : users){
+            if( user.getUsername().equalsIgnoreCase(input)||
+                user.getEmail().equalsIgnoreCase(input)
+            ){
+                return true;
+            }
+        }
+        return false;
+    }
 }
