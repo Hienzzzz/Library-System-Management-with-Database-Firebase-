2c46183b198d33da4b967d274200ea5f037b450f
Hienz <francispanogao@gmail.com>
2026-01-27 22:42:23 +0800

    Add unique book ID generation and addBook method
    Introduced a method to generate unique book IDs and updated the book addition logic to ensure uniqueness in BookService. Added a new AddBook.java file and an admin image for the add book screen.

--- mother_file/src/main/java/project/Firebase_backend/Book_backend/BookService.java
+++ mother_file/src/main/java/project/Firebase_backend/Book_backend/BookService.java
@@ -1,17 +1,52 @@
 package project.Firebase_backend.Book_backend;
 
+import java.util.Random;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.random.*;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 
 
 public class BookService {
+
 private static final DatabaseReference ref = FirebaseDatabase.getInstance().getReference("books");
 
 public static DatabaseReference getRef(){
     return ref;
 }
 
-public static void addBooks(Books book){
-    ref.push().setValueAsync(book);
+public static String generatedBookId(){
+    Random rand = new Random();
+    int number = 100000 + rand.nextInt(900000); // 6 digit to
+    return "BK-" + number;
+}
+
+public static void addBookWithUniqueId(Books book){
+    String newId = generatedBookId();
+    book.setBook_id(newId);
+
+    ref.orderByChild("book_id")
+    .equalTo(newId)
+    .addListenerForSingleValueEvent(new ValueEventListener() {
+        
+        @Override
+        public void onDataChange(DataSnapshot snapshot){
+            if(snapshot.exists()){
+                checkAndAdd(book);
+            }else{
+                ref.push().setValueAsync(book);
+            }
+        }
+
+        @Override
+        public void onCancelled(DatabaseError error){
+            System.out.println(error.getMessage());
+        }
+    });
 }
 }
